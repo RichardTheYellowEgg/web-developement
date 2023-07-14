d48563ecb16793606dc17c8efdcec92b84cc3f6e
unknown <richard.nguyen.ak@gmail.com>
2023-07-14 11:27:00 -0500

    magic

---
+++ ekart/Javascript/script.js
@@ -0,0 +1,9 @@
+document.querySelector(".heading").textContent = "magic";
+
+document.querySelector("#heading1").textContent = "soup";
+
+console.log(12 * 23);
+
+let beans = 23;
+
+console.log(beans)
