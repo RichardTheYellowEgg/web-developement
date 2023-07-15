d868314b723be2aa7dd4070cab7977ffceb4920f
unknown <richard.nguyen.ak@gmail.com>
2023-07-15 11:28:07 -0500

    what

--- ekart/Javascript/script.js
+++ ekart/Javascript/script.js
@@ -4,6 +4,25 @@ document.querySelector("#heading1").textContent = "soup";
 
 console.log(12 * 23);
 
-let beans = 23;
+// let rasd1kh2300_ = 12
 
-console.log(beans)
+// console.log(rasd1kh2300_)
+
+// let beans = 23;
+
+// console.log(beans)
+
+// const age = 1321;
+
+// console.log(age)
+
+// let age;
+
+// console.log(age)
+
+// let sds=null;
+
+// console.log(sds)
+
+let thing = 219390819803890149801249804239829089823423894289042989804n;
+console.log(thing)
