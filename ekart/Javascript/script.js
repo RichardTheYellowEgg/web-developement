4b8c83c9471027db044eeb8e1aa24d301a694e88
unknown <richard.nguyen.ak@gmail.com>
2023-07-18 11:36:48 -0500

    communism

--- ekart/Javascript/script.js
+++ ekart/Javascript/script.js
@@ -2,7 +2,7 @@ document.querySelector(".heading").textContent = "magic";
 
 document.querySelector("#heading1").textContent = "soup";
 
-console.log(12 * 23);
+// console.log(12 * 23);
 
 // let rasd1kh2300_ = 12
 
@@ -24,5 +24,52 @@ console.log(12 * 23);
 
 // console.log(sds)
 
-let thing = 219390819803890149801249804239829089823423894289042989804n;
-console.log(thing)
+// let thing = 219390819803890149801249804239829089823423894289042989804n;
+
+// console.log(thing)
+
+// asignment operators
+
+// let age;
+
+// age = 3 + 12;
+
+// log.console(age);
+
+// let age=6;
+
+// age += 4;
+
+// age=age+4;
+
+// console.log(age)
+
+// ++ is a unary operator
+
+// binary operator = +,-,*,/
+
+// unary operator = is implemented on a single operand
+
+// operand = number, strings, variables whitch are surrounding the operator
+
+//a = a + 1
+
+// a + 1 are the operand
+
+// age++;
+
+// age--;
+
+// assignment operators =, +=, -=, /=
+
+// if dividend < dividor then %  operator will give dividend as the answer
+
+// age += 1;
+
+// age =age+1;
+
+// // age++2; = this is nothing, it will give you an error
+
+// age += 2;
+
+// age=age+2;
