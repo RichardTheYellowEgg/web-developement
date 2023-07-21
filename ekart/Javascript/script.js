84cd1312b0f9f762071da82a9da84d8df9847b43
unknown <richard.nguyen.ak@gmail.com>
2023-07-21 11:42:20 -0500

    what

--- ekart/Javascript/script.js
+++ ekart/Javascript/script.js
@@ -119,46 +119,106 @@ document.querySelector("#heading1").textContent = "soup";
 // var x;
 
 // console.log(x);
-thing = 2;
-if( thing>1){
-    console.log("HES PULLING HIS DOCK OUT")
-}
-else if(thing>0){
-    console.log("dock 👍")
-}
-else{
-    console.log("👍")
-}
-if(2 == 3){
-    console.log("co co nut")
-}
-else if(2 == 3){
-    console.log("nut co co")
-}
-else{
-    console.log("nut nut nut")
-}
-// if(2 == 3){
-//     console.log("equal");
+// thing = 2;
+// if( thing>1){
+//     console.log("HES PULLING HIS DOCK OUT")
 // }
-// if(2 == 4){
-//     console.log("not equal");
-// }
-// if(2 == 5){
-//     console.log("dont know");
-// }
-// if(false == true){
-//     console.log("not possible");
-// }
-// if(true){
-//     console.log("true");
-// }
-// else if(5 != 3){
-//     console.log("not equal 5 and 3");
-// }
-// else if(4 != 4){
-//     console.log("not equal 4 and 4");
+// else if(thing>0){
+//     console.log("dock 👍")
 // }
 // else{
-//     console.log("Default value!!");
+//     console.log("👍")
+// }
+// if(2 == 3){
+//     console.log("co co nut")
+// }
+// else if(2 == 3){
+//     console.log("nut co co")
+// }
+// else{
+//     console.log("nut nut nut")
+// }
+// // if(2 == 3){
+// //     console.log("equal");
+// // }
+// // if(2 == 4){
+// //     console.log("not equal");
+// // }
+// // if(2 == 5){
+// //     console.log("dont know");
+// // }
+// // if(false == true){
+// //     console.log("not possible");
+// // }
+// // if(true){
+// //     console.log("true");
+// // }
+// // else if(5 != 3){
+// //     console.log("not equal 5 and 3");
+// // }
+// // else if(4 != 4){
+// //     console.log("not equal 4 and 4");
+// // }
+// // else{
+// //     console.log("Default value!!");
+// // }
+// x = 13
+// y = 2
+// z = 12
+// if(x == 3 && x < 4){
+//     console.log("yes");
+// }
+// if(x != 6){
+//     console.log("no")
+// }
+// if(x != 6 && y > 2){
+//     console.log("ye")
+// }
+// if(z !=1 || x == 3){
+//     console.log("how")
+// }
+// if(!(y<10)){
+//     console.log("where")
+// }
+// else if(x < 11 || x > 6){
+//     console.log("when")
+// }
+// else if(y < 10 || y <= 0){
+//     console.log("what")
+// }
+// else if(z == 0 && y == 5){
+//     console.log("why")
+// }
+// else{
+//     console.log("thing")
+// }
+// x = 12;
+// y = 3;
+// z = 11;
+// if(x == 2 && x < 3){
+//     console.log(x);
+// }
+// if(x != 5){
+//     console.log("whatever");
+// }
+// if(x != 5 && y >= 5){
+//     console.log("its gonna happen");
+// }
+// if(z != 0 || x == 2){
+//     console.log("you say");
+// }
+// if(!(y < 10)){
+//     console.log(y);
+// }
+// else if(x < 10 || x < 5){
+//     console.log("okkkkkkk");
+// }
+// else if(y < 10 || y <= 0){
+//     console.log("pikachu");
+// }
+// else if(z == 0 && y ==5){
+//     console.log("Pikaboo");
+// }
+// else{
+//     console.log("Default value");
 // }
