c330623569c641dafc053c8a5e8985303ff33a64
unknown <richard.nguyen.ak@gmail.com>
2023-08-04 11:39:39 -0500

    emanuel

--- not_netflix/not_netflix.js
+++ not_netflix/not_netflix.js
@@ -1,4 +1,4 @@
 document.querySelector("#text_1").style.textAlign = "center"
 document.querySelector("#text_1").style.marginTop = "300px"
 document.querySelector("#text_2").style.textAlign = "center"
-document.querySelector(".drop").style.marginLeft = "1400px"
+document.querySelector(".drop").style.marginLeft = "800px"
